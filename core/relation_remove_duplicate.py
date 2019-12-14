724725c7702cbfc60a67d6b5c3f182133c3cb164
kwonsungil <thuntherll@naver.com>
2019-12-14 17:53:17 +0900

    Update relation_remove_duplicate.py

--- core/relation_remove_duplicate.py
+++ core/relation_remove_duplicate.py
@@ -1,18 +1,12 @@
 import tensorflow as tf
+import numpy as np
+from core.relation_module import RelationModule
+import config
 
 
 class remove_duplicate:
-    def __init__(self, appearance_feature, geometric_feature, sample_roi, roi_score, roi_cls_loc, is_duplicated=False):
-        # sess = tf.Session()
-        # number of relation
-        self.Nr = 16
-        # appearance feature dimension
-        self.appearance_feature = 1024
-        # geo feature dimension
-        self.geo_feature = 64
-        # key feature dimension
-        self.key_feature = 64
-
+    def __init__(self, sample_roi, roi_score, roi_cls_loc):
+        sess = tf.Session()
         self.num_class = 20 + 1
         self.loc_normalize_mean = [0., 0., 0., 0.] * self.num_class
         self.loc_normalize_std = [0.1, 0.1, 0.2, 0.2] * self.num_class
@@ -20,166 +14,146 @@ class remove_duplicate:
         # pooling 된 RoI feature 값들
         # [-1, 7, 7, 1024]
         # N = sample_roi.shape[0]
-        N = 256
 
-        # [-1, 1]
+        # [-1, 2]
         # RoI 갯수만큼 softmax값이 들어 있음
+
         # roi_score
+        # 1번 idx가 object가 있는 경우
+        roi_range = tf.range(start=0, limit=tf.shape(roi_score)[0], delta=1)
+        roi_score_arg_max = tf.argmax(roi_score, 1)
+        roi_score_max = tf.reduce_max(roi_score, 1)
 
         # [-1, 4]
         # model이 예측한 좌표에 anchor x,y,w,h를 적용하고 x1,y1,x2,y2로 치환한 값
         # roi_cls_loc
 
-        # roi = at.totensor(sample_roi)
+        # object가 있다고 예측한 RoI
+        # 0 은 없다고 예측한 ROI
+        not_zero = tf.where(tf.not_equal(roi_score_arg_max, 0))
+        print(sess.run([roi_score_arg_max, roi_range, not_zero]))
+        not_zero = tf.squeeze(not_zero, 1)
+        # not_zero = tf.not_equal(roi_score_arg_max, 0)
 
-        # zero가 아닌 idx
-        not_zero = tf.where(tf.not_equal(roi_score, 0))
-        not_zero_size = tf.shape(not_zero)[0]
-
+        #TODO
         # RoI가 모두 0일 경우 제거하기 위해서
-        not_zero = tf.cond(not_zero_size==0, lambda: None, lambda: not_zero)
+        # not_zero = tf.cond(tf.equal(tf.shape(not_zero)[0], 0), lambda: None, lambda: not_zero)
 
-        if not_zero is not None:
-            roi_cls_loc = roi_cls_loc[not_zero]
-            roi_score = roi_score[not_zero]
-            sample_roi = sample_roi[not_zero]
+        # Object가 있다고 판단한 경우의 feature들만 가져옴
+        roi_cls_loc = tf.gather(roi_cls_loc, not_zero)
+        roi_score_max = tf.gather(roi_score_max, not_zero)
+        # not_zero는 object가 있다고 예측한 idx 정보
+        # roi_score_arg_max = tf.gather(roi_score_arg_max, not_zero)
+        sample_roi = tf.gather(sample_roi, not_zero)
+
+        sorted_idx = tf.argsort(roi_score_max, direction='DESCENDING')
+
+        # Object가 있든 feature들을 score값으로 sorting
+        roi_cls_loc = tf.gather(roi_cls_loc, sorted_idx, axis=0)
+        roi_score_max = tf.gather(roi_score_max, sorted_idx, axis=0)
+        not_zero = tf.gather(not_zero, sorted_idx, axis=0)
+        sample_roi = tf.gather(sample_roi, sorted_idx, axis=0)
+
+        roi_score_v, not_zero_v, sample_roi_v, sorted_idx_v, roi_score_max_v = sess.run([roi_score,
+                                                                                             not_zero,
+                                                                                             sample_roi,
+                                                                                             sorted_idx,
+                                                                                             roi_score_max,
+                                                                                             ])
+        print('roi_score_v : ', roi_score_v[:10])
+        print('not_zero_v : ', not_zero_v[:10])
+        print('not_zero_v : ', not_zero_v.shape)
+        print('sample_roi_v', sample_roi_v.shape)
+        print('sorted_idx_v', sorted_idx_v[:10])
+        # sorting 된 socre
+        print('roi_score_max_v', roi_score_max_v[:10])
+        print('roi_score_max_v', roi_score_max_v.shape)
+
+        # [numver of RoI, 1024] (score 값이 zero가 없을 때)
+        # rank dim은 후보 feature의 개수 많큼 생성해야함
+        nms_rank_embedding = self.RankEmbedding(rank_dim=tf.shape(sample_roi)[0])
+        # backpropagation을 해야 할까...? 논문에서는 아닌 거 같음
+        nms_rank = tf.layers.dense(nms_rank_embedding, 128, use_bias=True,
+                                   kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
+                                   activation=tf.nn.relu)
+
+        roi_feat_embedding = tf.layers.dense(sample_roi, 128, use_bias=True,
+                                             kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
+                                             activation=tf.nn.relu)
+
+        nms_embedding_feat = tf.add(nms_rank, roi_feat_embedding)
+        # sorting된 bbox정보를 넣고 positional embedding을 구함
+        nms_logit = RelationModule(nms_embedding_feat, roi_cls_loc)
+
+        print('nms_rank_embedding : ', nms_rank_embedding)
+        print('nms_rank : ', nms_rank)
+        print('roi_feat_embedding : ', roi_feat_embedding)
+        print('nms_embedding_feat : ', nms_embedding_feat)
+        print('nms_logit.result : ', nms_logit.result)
+
+        s1 = tf.layers.dense(nms_logit.result, 1, use_bias=True,
+                                    kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
+                                    activation=tf.nn.sigmoid)
+
+        nms_scores = s1 * roi_score_max
+        # print('s1 : ', s1)
+        # print('roi_score_max : ', roi_score_max)
+        # print('nms_scores : ', nms_scores)
+
+        self.nms_scores = nms_scores
+        self.not_zero = not_zero
+        self.roi_cls_loc = roi_cls_loc
+
+        sess.run(tf.global_variables_initializer())
+        a, b, c = sess.run([nms_scores, not_zero, roi_cls_loc])
+        print(a.shape)
+        print(b.shape)
+        print(c.shape)
 
 
-        # score 순으로 정렬 필요
-        range = tf.range(start=0, limit=N, delta=1)
-        print(range)
-        roi_score_argmax = tf.argmax(roi_score, axis=1)
-        print(roi_score_argmax)
-        # print(sess.run([roi_score_argmax]))
 
-
-
-        # self.NMS_rank = tf.layers.dense(appearance_feature, 128, use_bias=True,
-        #                            kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-        #                            activation=tf.nn.relu)
-        # self.NMS_logit = tf.layers.dense(appearance_feature, 1, use_bias=True,
-        #                                 kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-        #                                 activation=tf.nn.relu)
-        # self.RoI = tf.layers.dense(appearance_feature, 128, use_bias=True,
-        #                            kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-        #                            activation=tf.nn.relu)
-        # result = []
-        # for num in range(self.Nr):
-        #     result.append(self.relation(appearance_feature, geometric_feature))
+    def RankEmbedding(self, rank_dim, feat_dim=1024, wave_len=1000.):
+        # # numpy implementation
+        # rank_range = np.arange(0, rank_dim)
+        # feat_range = np.arange(feat_dim / 2)
+        # dim_mat = feat_range / (feat_dim / 2)
+        # dim_mat = 1. / (np.power(wave_len, dim_mat))
         #
-        # result = tf.concat(result, axis=1)
+        # # (1, 512)
+        # dim_mat = np.reshape(dim_mat, [1, -1])
+        # # (128, 1)
+        # rank_mat = np.reshape(rank_range, [-1, 1])
+        # # (128, 512)
+        # mul_mat = rank_mat * dim_mat
+        # sin_mat = np.sin(mul_mat)
+        # cos_mat = np.cos(mul_mat)
+        # # [128, 1024]
+        # embedding = np.concatenate([sin_mat, cos_mat], -1)
 
-    def relation(self, appearance_feature, geometric_feature):
+        rank_range = tf.range(start=0, limit=rank_dim, delta=1)
+        feat_range = tf.range(start=0, limit=feat_dim / 2, delta=1)
+        dim_mat = feat_range / (feat_dim / 2)
+        dim_mat = 1. / (tf.pow(wave_len, dim_mat))
 
-        # (number of RoI * 7 * 7 * 1024,, 1024)
-        num_roi = appearance_feature.shape[0]
-        Wk = tf.layers.dense(appearance_feature, self.key_feature, use_bias=True,
-                             kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-                             activation=tf.nn.relu)
-        Wq = tf.layers.dense(appearance_feature, self.key_feature, use_bias=True,
-                             kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-                             activation=tf.nn.relu)
-        Wv = tf.layers.dense(appearance_feature, self.key_feature, use_bias=True,
-                             kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-                             activation=tf.nn.relu)
-        Wg = tf.layers.dense(tf.nn.relu(self.PositionalEmbedding(geometric_feature)), 1, use_bias=True,
-                             kernel_initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
-                             activation=tf.nn.relu)
-
-        Wk = tf.reshape(Wk, [1, num_roi, self.key_feature])
-        Wq = tf.reshape(Wq, [num_roi, 1, self.key_feature])
-        print('Wk : ', Wk)
-        print('Wq : ', Wq)
-        scaled_dot = tf.reduce_sum([Wk * Wq], axis=-1) / tf.sqrt(tf.to_float(self.key_feature))
-        # scaled_dot = Wk * Wq / tf.sqrt(tf.to_float(self.key_feature))
-        print('scaled_dot : ', scaled_dot)
-        print('Wg : ', Wg)
-        Wg = tf.reshape(Wg, [num_roi, num_roi])
-        print('Wg : ', Wg)
-        Wa = tf.reshape(scaled_dot, [num_roi, num_roi])
-
-        Wmn = tf.log(tf.clip_by_value(Wg, clip_value_min=1e-6, clip_value_max=100000000)) + Wa
-        Wmn = tf.nn.softmax(Wmn, axis=1)
-        print('Wmn : ', Wmn)
-        Wmn = tf.reshape(Wmn, [num_roi, num_roi, 1])
-        print('Wmn : ', Wmn)
-        print('Wv : ', Wv)
-        Wv = tf.reshape(Wv, [num_roi, 1, -1])
-        print('Wv : ', Wv)
-
-        return tf.reduce_sum(Wmn * Wv, axis=-2)
-
-    def PositionalEmbedding(self, geometric_feature, dim_g=64, wave_len=1000.):
-        # (number of RoI, coordinate of Roi)
-        # x_min, y_min, x_max, y_max = torch.chunk(f_g, 4, dim=1)
-        xmin, ymin, xmax, ymax = tf.split(value=geometric_feature, num_or_size_splits=4, axis=1)
-
-        x = (xmin + xmax) * 0.5
-        y = (ymin + ymax) * 0.5
-        w = (xmax - xmin) + 1.
-        h = (ymax - ymin) + 1.
-
-        delta_x = x - tf.reshape(x, [1, -1])
-        print(delta_x)
-        # delta_x = torch.clamp(tf.abs(delta_x / w), min=1e-3)
-        # delta_x = torch.log(delta_x)
-        delta_x = tf.clip_by_value(tf.abs(delta_x / w), clip_value_min=1e-3, clip_value_max=100000000)
-        delta_x = tf.log(delta_x)
-
-        delta_y = y - tf.reshape(y, [1, -1])
-        delta_y = tf.clip_by_value(tf.abs(delta_y / h), clip_value_min=1e-3, clip_value_max=100000000)
-        delta_y = tf.log(delta_y)
-
-        delta_w = tf.log(w / tf.reshape(w, [1, -1]))
-        delta_h = tf.log(h / tf.reshape(h, [1, -1]))
-        shape = delta_h.shape
-
-        delta_x = tf.reshape(delta_x, [shape[0], shape[1], 1])
-        print(delta_x)
-        delta_y = tf.reshape(delta_y, [shape[0], shape[1], 1])
-        delta_w = tf.reshape(delta_w, [shape[0], shape[1], 1])
-        delta_h = tf.reshape(delta_h, [shape[0], shape[1], 1])
-
-        position_mat = tf.concat([delta_x, delta_y, delta_w, delta_h], axis=-1)
-
-        # feat_range = torch.arange(dim_g / 8).cuda()
-        feat_range = tf.range(dim_g / 8)
-        dim_mat = feat_range / (dim_g / 8)
-        # dim_mat = tf.to_int32(dim_mat)
-        print(dim_mat)
-        dim_mat = 1 / (tf.pow(wave_len, dim_mat))
-
-        dim_mat = tf.reshape(dim_mat, [1, 1, 1, -1])
-        position_mat = tf.reshape(position_mat, [shape[0], shape[1], 4, -1])
-        # dim_mat = dim_mat.view(1, 1, 1, -1)
-        # position_mat = position_mat.view(shape[0], shape[1], 4, -1)
-        position_mat = 100. * position_mat
-
-        mul_mat = position_mat * dim_mat
-        mul_mat = tf.reshape(mul_mat, [shape[0], shape[1], -1])
-        # mul_mat = mul_mat.view(shape[0], shape[1], -1)
+        # (1, 512)
+        dim_mat = tf.reshape(dim_mat, [1, -1])
+        # (128, 1)
+        rank_mat = tf.reshape(rank_range, [-1, 1])
+        # (128, 512)
+        mul_mat = tf.to_float(rank_mat) * dim_mat
         sin_mat = tf.sin(mul_mat)
         cos_mat = tf.cos(mul_mat)
-        embedding = tf.concat((sin_mat, cos_mat), -1)
+        # [128, 1024]
+        embedding = tf.concat([sin_mat, cos_mat], -1)
 
         return embedding
 
-    def clip_boxes(self, boxes, img_width, img_height):
-          img_width = tf.cast(img_width, tf.float32)
-          img_height = tf.cast(img_height, tf.float32)
-          b0 = tf.maximum(tf.minimum(boxes[:, 0], img_width - 1), 0.0)
-          b1 = tf.maximum(tf.minimum(boxes[:, 1], img_height - 1), 0.0)
-          b2 = tf.maximum(tf.minimum(boxes[:, 2], img_width - 1), 0.0)
-          b3 = tf.maximum(tf.minimum(boxes[:, 3], img_height - 1), 0.0)
-          return tf.stack([b0, b1, b2, b3], axis=1)
-
 
 if __name__ == '__main__':
-    appearance_feature = tf.random_normal(shape=[256, 7, 7, 1024])
-    geometric_feature = tf.random_normal(shape=[256, 4])
+
     sample_roi = tf.random_normal(shape=[256, 7, 7, 1024])
-    roi_score = tf.random_normal(shape=[256, 1])
+    roi_score = tf.random_normal(shape=[256, 2])
     roi_cls_loc = tf.random_normal(shape=[256, 4])
 
-    appearance_feature = tf.layers.flatten(appearance_feature)
-    rm = remove_duplicate( appearance_feature, geometric_feature, sample_roi, roi_score, roi_cls_loc)
+    sample_roi = tf.layers.flatten(sample_roi)
+    rm = remove_duplicate(sample_roi, roi_score, roi_cls_loc)
